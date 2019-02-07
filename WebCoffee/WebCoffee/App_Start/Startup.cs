ee18e482afea2495fc926a0e1f951f11df123eec
Руслан Абдрахманов <vip.ra.1999@hotmail.com>
2019-02-07 18:09:37 +0400

    #4 Setting Identity
    В главный слой добавлены нужные nuget-пакеты.
    Добавлена ссылка на слой BLL.
    Проведена донастройка Identity.

---
+++ WebCoffee/WebCoffee/App_Start/Startup.cs
@@ -0,0 +1,34 @@
+﻿using BLL.Interfaces;
+using BLL.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(WebCoffee.App_Start.Startup))]
+
+namespace WebCoffee.App_Start
+{
+    public class Startup
+    {
+        IServiceCreator serviceCreator = new ServiceCreator();                      //С помощью фабрики сервисов здесь создается сервис для работы с сервисами
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IUserService>(CreateUserService);              //Сервис региструется контекстом OWIN
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return serviceCreator.CreateUserService("DefaultConnection");
+        }
+    }
+}
---
+++ WebCoffee/WebCoffee/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using BLL.DTO;
+using BLL.Infrastructure;
+using BLL.Interfaces;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebCoffee.Models;
+
+namespace WebCoffee.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<IUserService>();
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            await SetInitialDataAsync();
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO { Email = model.Email, Password = model.Password };
+                ClaimsIdentity claim = await UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Неверный логин или пароль.");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            await SetInitialDataAsync();
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO
+                {
+                    Email = model.Email,
+                    Password = model.Password,
+                    Address = model.Address,
+                    Name = model.Name,
+                    Role = "user"
+                };
+                OperationDetails operationDetails = await UserService.Create(userDto);
+                if (operationDetails.Succedeed)
+                    return View("SuccessRegister");
+                else
+                    ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+            }
+            return View(model);
+        }
+        private async Task SetInitialDataAsync()
+        {
+            await UserService.SetInitialData(new UserDTO
+            {
+                Email = "vip.ra.19992@gmail.com",
+                UserName = "admin",
+                Password = "admin",
+                Name = "Админов Админ Админович",
+                Address = "ул. Админова, д.30, кв.75",
+                Role = "admin",
+            }, new List<string> { "user", "admin" });
+        }
+    }
+}
