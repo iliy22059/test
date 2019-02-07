17132e5c027647c3836745c34920103816e53f24
Руслан Абдрахманов <vip.ra.1999@hotmail.com>
2019-02-07 17:45:07 +0400

    #3 Create BLL
    Создан слой бизнес логики приложения.
    Добавлены нужные nuget-пакеты.
    Добавлена ссылка на слой DAL.
    Созданы нужные классы и интерфейсы, в том числе связывающие со слоем доступа к данным.

---
+++ WebCoffee/BLL/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using BLL.DTO;
+using BLL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IUserService : IDisposable
+    {
+        Task<OperationDetails> Create(UserDTO userDto);             //Создание пользователей
+        Task<ClaimsIdentity> Authenticate(UserDTO userDto);         //Аутентификация пользователей
+        Task SetInitialData(UserDTO adminDto, List<string> roles);  //Установка начальных данных о БД администратора и списке ролей
+    }
+}
